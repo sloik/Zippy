674a986cf27b6b4ec018d4be5bbbe4d1c401989d
Edward Maliniak <mail@mail.com>
2022-08-21 13:32:04 +0200

    Switches to guard.

--- Sources/Zippy/ZipOptional.swift
+++ Sources/Zippy/ZipOptional.swift
@@ -6,112 +6,146 @@ import Foundation
 // MARK: Zip on Optionals
 @discardableResult
 public func zip<A, B>(
-  _ a: A?,
-  _ b: B?
+    _ a: A?,
+    _ b: B?
 ) -> (A, B)? {
-    guard let a = a, let b = b else { return .none }
+    guard
+        let a = a, let b = b
+    else { return .none }
 
     return (a,b)
 }
 
 @discardableResult
 public func zip<A, B, C>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?
-  ) -> (A, B, C)? {
-  return zip(zip(a, b), c).map { ($0.0, $0.1, $1) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?
+) -> (A, B, C)? {
+    guard
+        let a = a, let b = b, let c = c
+    else { return .none }
+
+    return (a,b,c)
 }
 
 @discardableResult
 public func zip<A, B, C, D>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?,
-  _ d: D?
-  ) -> (A, B, C, D)? {
-  return zip(zip(a, b), c, d).map { ($0.0, $0.1, $1, $2) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?,
+    _ d: D?
+) -> (A, B, C, D)? {
+    guard
+        let a = a, let b = b, let c = c, let d = d
+    else { return .none }
+
+    return (a,b,c,d)
 }
 
 @discardableResult
 public func zip<A, B, C, D, E>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?,
-  _ d: D?,
-  _ e: E?
-  ) -> (A, B, C, D, E)? {
-  return zip(zip(a, b), c, d, e).map { ($0.0, $0.1, $1, $2, $3) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?,
+    _ d: D?,
+    _ e: E?
+) -> (A, B, C, D, E)? {
+    guard
+        let a = a, let b = b, let c = c, let d = d, let e = e
+    else { return .none }
+
+    return (a,b,c,d,e)
 }
 
 @discardableResult
 public func zip<A, B, C, D, E, F>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?,
-  _ d: D?,
-  _ e: E?,
-  _ f: F?
-  ) -> (A, B, C, D, E, F)? {
-  return zip(zip(a, b), c, d, e, f).map { ($0.0, $0.1, $1, $2, $3, $4) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?,
+    _ d: D?,
+    _ e: E?,
+    _ f: F?
+) -> (A, B, C, D, E, F)? {
+    guard
+        let a = a, let b = b, let c = c, let d = d, let e = e, let f = f
+    else { return .none }
+
+    return (a,b,c,d,e,f)
 }
 
 @discardableResult
 public func zip<A, B, C, D, E, F, G>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?,
-  _ d: D?,
-  _ e: E?,
-  _ f: F?,
-  _ g: G?
-  ) -> (A, B, C, D, E, F, G)? {
-  return zip(zip(a, b), c, d, e, f, g).map { ($0.0, $0.1, $1, $2, $3, $4, $5) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?,
+    _ d: D?,
+    _ e: E?,
+    _ f: F?,
+    _ g: G?
+) -> (A, B, C, D, E, F, G)? {
+    guard
+        let a = a, let b = b, let c = c, let d = d, let e = e, let f = f, let g = g
+    else { return .none }
+
+    return (a,b,c,d,e,f,g)
 }
 
 @discardableResult
 public func zip<A, B, C, D, E, F, G, H>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?,
-  _ d: D?,
-  _ e: E?,
-  _ f: F?,
-  _ g: G?,
-  _ h: H?
-  ) -> (A, B, C, D, E, F, G, H)? {
-  return zip(zip(a, b), c, d, e, f, g, h).map { ($0.0, $0.1, $1, $2, $3, $4, $5, $6) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?,
+    _ d: D?,
+    _ e: E?,
+    _ f: F?,
+    _ g: G?,
+    _ h: H?
+) -> (A, B, C, D, E, F, G, H)? {
+    guard
+        let a = a, let b = b, let c = c, let d = d, let e = e, let f = f, let g = g, let h = h
+    else { return .none }
+
+    return (a,b,c,d,e,f,g,h)
 }
 
 @discardableResult
 public func zip<A, B, C, D, E, F, G, H, I>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?,
-  _ d: D?,
-  _ e: E?,
-  _ f: F?,
-  _ g: G?,
-  _ h: H?,
-  _ i: I?
-  ) -> (A, B, C, D, E, F, G, H, I)? {
-  return zip(zip(a, b), c, d, e, f, g, h, i).map { ($0.0, $0.1, $1, $2, $3, $4, $5, $6, $7) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?,
+    _ d: D?,
+    _ e: E?,
+    _ f: F?,
+    _ g: G?,
+    _ h: H?,
+    _ i: I?
+) -> (A, B, C, D, E, F, G, H, I)? {
+    guard
+        let a = a, let b = b, let c = c, let d = d, let e = e, let f = f, let g = g, let h = h, let i = i
+    else { return .none }
+
+    return (a,b,c,d,e,f,g,h,i)
 }
 
 @discardableResult
 public func zip<A, B, C, D, E, F, G, H, I, J>(
-  _ a: A?,
-  _ b: B?,
-  _ c: C?,
-  _ d: D?,
-  _ e: E?,
-  _ f: F?,
-  _ g: G?,
-  _ h: H?,
-  _ i: I?,
-  _ j: J?
-  ) -> (A, B, C, D, E, F, G, H, I, J)? {
-  return zip(zip(a, b), c, d, e, f, g, h, i, j).map { ($0.0, $0.1, $1, $2, $3, $4, $5, $6, $7, $8) }
+    _ a: A?,
+    _ b: B?,
+    _ c: C?,
+    _ d: D?,
+    _ e: E?,
+    _ f: F?,
+    _ g: G?,
+    _ h: H?,
+    _ i: I?,
+    _ j: J?
+) -> (A, B, C, D, E, F, G, H, I, J)? {
+    guard
+        let a = a, let b = b, let c = c, let d = d, let e = e, let f = f, let g = g, let h = h, let i = i, let j = j
+    else { return .none }
+
+    return (a,b,c,d,e,f,g,h,i,j)
 }
 
 // MARK: -
